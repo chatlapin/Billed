d0d62cc70bb5c46066bedbab93917b02b616f71e
chatlapin <choh9323@gmail.com>
2024-07-07 19:31:37 +0200

    Update NewBill.js

--- Billed-app-FR-Front-main/src/containers/NewBill.js
+++ Billed-app-FR-Front-main/src/containers/NewBill.js
@@ -57,9 +57,9 @@ export default class NewBill {
     const bill = {
       email,
       type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
-      name:  e.target.querySelector(`input[data-testid="expense-name"]`).value,
+      name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
       amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
-      date:  e.target.querySelector(`input[data-testid="datepicker"]`).value,
+      date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
       vat: e.target.querySelector(`input[data-testid="vat"]`).value,
       pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
       commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
@@ -75,12 +75,12 @@ export default class NewBill {
   updateBill = (bill) => {
     if (this.store) {
       this.store
-      .bills()
-      .update({data: JSON.stringify(bill), selector: this.billId})
-      .then(() => {
-        this.onNavigate(ROUTES_PATH['Bills'])
-      })
-      .catch(error => console.error(error))
+       .bills()
+       .update({ data: JSON.stringify(bill), selector: this.billId})
+       .then(() => {
+         this.onNavigate(ROUTES_PATH['Bills'])
+       })
+       .catch(error => console.error(error))
     }
   }
 }
