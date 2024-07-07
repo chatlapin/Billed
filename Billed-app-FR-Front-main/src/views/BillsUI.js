ba28d877844a9b3cfc049113e55bd05e59fac60f
chatlapin <choh9323@gmail.com>
2024-07-07 19:11:23 +0200

    Update BillsUI.js
    data is an array of bills. add sorting.
    1)  data.sort((a, b) => new Date(b.date) - new Date(a.date))
    2) return (data && data.length) ? data.sort((a, b) => new Date(b.date) - new Date(a.date)).map(bill => row(bill)).join("") : ""

--- Billed-app-FR-Front-main/src/views/BillsUI.js
+++ Billed-app-FR-Front-main/src/views/BillsUI.js
@@ -24,6 +24,8 @@ const rows = (data) => {//data is an array of bills. add sorting
   return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
 }
 
+//  return (data && data.length) ? data.sort((a, b) => new Date(b.date) - new Date(a.date)).map(bill => row(bill)).join("") : ""
+
 export default ({ data: bills, loading, error }) => {
   
   const modal = () => (`
