615eb39326d100ea76265b4cbdcb7f2db4f2959b
chatlapin <choh9323@gmail.com>
2024-07-12 16:41:52 +0200

    lcov test and admin login

--- Billed-app-FR-Front/src/containers/Login.js
+++ Billed-app-FR-Front/src/containers/Login.js
@@ -23,7 +23,6 @@ export default class Login {
       password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
       status: "connected"
     }
-    console.log(JSON.stringify(user));
     this.localStorage.setItem("user", JSON.stringify(user))
     this.login(user)
       .catch(
@@ -42,8 +41,8 @@ export default class Login {
     e.preventDefault()
     const user = {
       type: "Admin",
-      email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
-      password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
+      email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
+      password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
       status: "connected"
     }
     this.localStorage.setItem("user", JSON.stringify(user))
