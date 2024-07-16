c744c939a18e081b03dba74b1bf819a67b2049cf
chatlapin <choh9323@gmail.com>
2024-07-16 20:46:57 +0200

    bill order (desc)

--- Billed-app-FR-Front/src/views/BillsUI.js
+++ Billed-app-FR-Front/src/views/BillsUI.js
@@ -20,7 +20,10 @@ const row = (bill) => {
   }
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+ // fixing bill order. return (data && data.length) ? data.map(bill => row(bill)).join("") : ""}
+ data.sort((a, b) => (new Date(a.date) < new Date(b.date) ? 1 : -1));
+ return (data && data.length) ? data.sort((a, b) => new Date(b.date) - new Date(a.date)).map(bill => row(bill)).join("") : ""
+ 
 }
 
 export default ({ data: bills, loading, error }) => {
